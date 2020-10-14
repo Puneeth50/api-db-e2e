0e48bbbb66a5c3a621f7dca1d90bee8846c8e7ed
puneeth <puneeth50@gmail.com>
2020-10-14 15:06:25 +0100

    added basic skelton of the project and spring plugin dep

---
+++ api/src/main/java/com/api/db/e2e/api/restApi.java
@@ -0,0 +1,7 @@
+package com.api.db.e2e.api;
+
+/**
+ * Created by kedika on 14/10/2020.
+ */
+public class restApi {
+}
---
+++ api/src/main/java/com/api/db/e2e/apiApplication.java
@@ -0,0 +1,7 @@
+package com.api.db.e2e;
+
+/**
+ * Created by kedika on 14/10/2020.
+ */
+public class apiApplication {
+}
